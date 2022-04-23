67431889ed2e019e50aa6737b75972b54ced3123
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2022-04-22 20:21:07 -0400

    wip

--- src/main/scala/com/michaelmaysonet74/handsonscala/Main.scala
+++ src/main/scala/com/michaelmaysonet74/handsonscala/Main.scala
@@ -3,7 +3,7 @@ package com.michaelmaysonet74.handsonscala
 import com.michaelmaysonet74.handsonscala.chapters._
 
 object Main extends App {
-  // Chapter3.execute()
+  Chapter3.execute()
   Chapter4.execute()
-  // Chapter12.execute()
+  Chapter12.execute()
 }
