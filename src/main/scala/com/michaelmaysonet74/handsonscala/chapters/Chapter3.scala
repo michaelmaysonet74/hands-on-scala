50f7ddff172a689e54ac9ff06d41b25cc5b62e50
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2022-05-02 13:01:02 -0400

    wip

--- src/main/scala/com/michaelmaysonet74/handsonscala/chapters/Chapter3.scala
+++ src/main/scala/com/michaelmaysonet74/handsonscala/chapters/Chapter3.scala
@@ -19,13 +19,13 @@ final case class Chapter3()(implicit val ec: ExecutionContext) {
 
     printMessages(
       List(
-        new Msg(0, None, "Hello"),
-        new Msg(1, Some(0), "World"),
-        new Msg(2, None, "I am Cow"),
-        new Msg(3, Some(2), "Hear me moo"),
-        new Msg(4, Some(2), "Here I stand"),
-        new Msg(5, Some(2), "I am Cow"),
-        new Msg(6, Some(5), "Here me moo, moo")
+        Msg(0, None, "Hello"),
+        Msg(1, Some(0), "World"),
+        Msg(2, None, "I am Cow"),
+        Msg(3, Some(2), "Hear me moo"),
+        Msg(4, Some(2), "Here I stand"),
+        Msg(5, Some(2), "I am Cow"),
+        Msg(6, Some(5), "Here me moo, moo")
       )
     )
 
